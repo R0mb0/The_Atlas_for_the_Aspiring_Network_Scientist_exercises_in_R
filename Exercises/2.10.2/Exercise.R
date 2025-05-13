1ed5886410182112d944320bd5e2834ecebb691b
Francesco Rombaldoni <f.rombaldoni@campus.uniurb.it>
2025-05-13 22:32:15 +0200

    working on exercises

---
+++ Exercises/2.10.2/Exercise.R
@@ -0,0 +1,14 @@
+# 60% of the emails hitting my inbox is spam. You design a phenomenal
+#spam filter which is able to tell me, with 98% accuracy,
+#whether an email is spam or not: if an email is not spam, the system
+#has a 98% probability of saying so. The filter knows 60% of
+#emails are spam and so it will flag 60% of my emails. Suppose
+#that, at the end of the week, I look in my spam box and see 963
+#emails. Use Bayes’ Theorem to calculate how many of those 963
+#emails in my spam box I should suspect to be non-spam.
+
+percent <- function(percentValue, value) {
+  return((percentValue / 100) * value)
+}
+
+totMails <- 963
