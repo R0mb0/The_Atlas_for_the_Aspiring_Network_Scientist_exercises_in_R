0da293b1e59fe7837a6e10714401cc8a6ec2dcd2
Francesco Rombaldoni <f.rombaldoni@campus.uniurb.it>
2025-05-19 15:47:47 +0200

    Added exercise

---
+++ Exercises/5.8.2/Problem.R
@@ -0,0 +1,17 @@
+# Suppose:
+#
+#  A=(1 0)
+#     0 2
+#
+#  B=(3 0)
+#     0 -1
+#
+#  Are these two transformations commutative? Does applying A
+#  first and B second lead to the same transformation as applying B
+#  first and A second?
+
+# Defining the matrices
+A <- matrix(c(1, 0, 0, 2), nrow = 2, byrow = TRUE)
+B <- matrix(c(3, 0, 0, -1), nrow = 2, byrow = TRUE)
+
+# Write here the solution
---
+++ Exercises/5.8.2/Solution.R
@@ -0,0 +1,28 @@
+# Suppose:
+#
+#  A=(1 0)
+#     0 2
+#
+#  B=(3 0)
+#     0 -1
+#
+#  Are these two transformations commutative? Does applying A
+#  first and B second lead to the same transformation as applying B
+#  first and A second?
+
+# Defining the matrices
+A <- matrix(c(1, 0, 0, 2), nrow = 2, byrow = TRUE)
+B <- matrix(c(3, 0, 0, -1), nrow = 2, byrow = TRUE)
+
+# Solution
+
+# Computing A * B and B * A (matrix multiplication)
+AB <- A %*% B
+BA <- B %*% A
+
+# Print the results
+cat("A %*% B = ", AB, "\n")
+cat("B %*% A = ", BA, "\n")
+
+# Are they equal?
+cat("Are A and B commutative? ", all(AB == BA), "\n")
