a168dc01f98a78428321c85817b3ec7fb9189808
Francesco Rombaldoni <f.rombaldoni@campus.uniurb.it>
2025-05-19 14:32:47 +0200

    Added exercise

---
+++ Exercises/4.6.1/Solution.R
@@ -0,0 +1,26 @@
+# Generate a random vector with 100 normally distributed random
+# values (with zero average and standard deviation of one). Imple-
+# ment the softmax function. Plot the result with the original vector
+# on the x axis and the softmax output on the y axis.
+
+set.seed(42)
+
+# Solution
+
+# Generating the vector 
+x <- rnorm(100, mean = 0, sd = 1)
+
+# Implementing softmax function
+softmax <- function(z) {
+  exp_z <- exp(z - max(z)) # For numerical stability
+  exp_z / sum(exp_z)
+}
+
+y <- softmax(x)
+
+# Drawing the results (not required)
+plot(x, y, 
+     main = "Softmax Output vs Original Vector", 
+     xlab = "Original Values", 
+     ylab = "Softmax Output", 
+     pch = 19, col = "blue")
