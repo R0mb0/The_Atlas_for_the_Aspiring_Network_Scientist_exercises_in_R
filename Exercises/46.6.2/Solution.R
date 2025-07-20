b70c00156bb978db0d00056242cfc9f358ecf1fe
Francesco Rombaldoni <f.rombaldoni@campus.uniurb.it>
2025-07-20 10:15:42 +0200

    Added exercise

---
+++ Exercises/46.6.2/Solution.R
@@ -0,0 +1,19 @@
+# The table at http://www.networkatlas.eu/exercises/46/2/
+# diffusion.txt contains the information of which node (first
+# column) influenced which other node (second column). Use it
+# to summarize the graph by keeping only the edges used by the
+# spreading process.
+
+library(igraph)
+library(here)
+
+# Loading the diffusion edge list from the file in the script folder
+diffusion_edges <- read.table(here("diffusion.txt"), header = FALSE)
+
+# Building the graph using the edges from the diffusion process
+diffusion_graph <- graph_from_edgelist(as.matrix(diffusion_edges), directed = TRUE)
+
+# Solution 
+
+# Printing the summary of the diffusion graph to show only the edges used by the spreading process
+print(diffusion_graph)
